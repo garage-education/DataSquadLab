f0a10e9f9bc7e7e97215f98dc3b7a217c7a94700
mustafaalaa.mohamed@gmail.com <mustafaalaa.mohamed@gmail.com>
2024-03-03 21:31:19 +0000

    Update S3 prefix name

--- aws/eu-west-1/02_s3/locals.tf
+++ aws/eu-west-1/02_s3/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  name_prefix = "${var.prefix}-s3-${var.environment}-${var.region}-${var.name}"
+  name_prefix = "${var.prefix}s3-${var.environment}-${var.region}-${var.name}"
 
   env = {
     account_id  = data.aws_caller_identity.current.account_id
