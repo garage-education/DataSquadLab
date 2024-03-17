6a5d524120b711be08ec98945c6bba7cdee9cc00
mustafaalaa.mohamed@gmail.com <mustafaalaa.mohamed@gmail.com>
2024-03-17 23:18:27 +0000

    UPDATE: rds admin password sm name

--- aws/eu-west-1/06_eks_apps/k8s_secret_creation.tf
+++ aws/eu-west-1/06_eks_apps/k8s_secret_creation.tf
@@ -4,29 +4,29 @@ module "rds_admin_k8s_external_secret" {
   external_secret_name = "${local.name_prefix}-rds-admin-external-secret"
   secret_map           = [
     {
-      external_sm_name          = "tf-petclinic-db-secret"
-      external_sm_name_key      = "POSTGRES_HOST"
-      k8s_property_key = "PGHOST"
+      external_sm_name     = "tf-rds-admin-secret"
+      external_sm_name_key = "POSTGRES_HOST"
+      k8s_property_key     = "PGHOST"
     },
     {
-      external_sm_name          = "tf-petclinic-db-secret"
-      external_sm_name_key      = "PGDATABASE"
-      k8s_property_key = "POSTGRES_DB"
+      external_sm_name     = "tf-rds-admin-secret"
+      external_sm_name_key = "PGDATABASE"
+      k8s_property_key     = "POSTGRES_DB"
     },
     {
-      external_sm_name          = "tf-petclinic-db-secret"
-      external_sm_name_key      = "POSTGRES_PORT"
-      k8s_property_key = "PGPORT"
+      external_sm_name     = "tf-rds-admin-secret"
+      external_sm_name_key = "POSTGRES_PORT"
+      k8s_property_key     = "PGPORT"
     },
     {
-      external_sm_name          = "rds!db-390eb28a-2563-4d84-b9f1-9b3205f59365"
-      external_sm_name_key      = "POSTGRES_PORT"
-      k8s_property_key = "password"
+      external_sm_name     = "rds!db-390eb28a-2563-4d84-b9f1-9b3205f59365"
+      external_sm_name_key = "POSTGRES_PORT"
+      k8s_property_key     = "password"
     },
     {
-      external_sm_name          = "rds!db-390eb28a-2563-4d84-b9f1-9b3205f59365"
-      external_sm_name_key      = "POSTGRES_PORT"
-      k8s_property_key = "username"
+      external_sm_name     = "rds!db-390eb28a-2563-4d84-b9f1-9b3205f59365"
+      external_sm_name_key = "POSTGRES_PORT"
+      k8s_property_key     = "username"
     }
   ]
 
@@ -41,19 +41,19 @@ module "metabase_k8s_external_secret" {
   external_secret_name = "${local.name_prefix}-metabase-external-secret"
   secret_map           = [
     {
-      external_sm_name          = "tf-metabase-db-secret"
-      external_sm_name_key      = "MB_DB_USER"
-      k8s_property_key = "database_user"
+      external_sm_name     = "tf-metabase-db-secret"
+      external_sm_name_key = "MB_DB_USER"
+      k8s_property_key     = "database_user"
     },
     {
-      external_sm_name          = "tf-metabase-db-secret"
-      external_sm_name_key      = "MB_DB_PASS"
-      k8s_property_key = "database_password"
+      external_sm_name     = "tf-metabase-db-secret"
+      external_sm_name_key = "MB_DB_PASS"
+      k8s_property_key     = "database_password"
     },
     {
-      external_sm_name          = "tf-metabase-db-secret"
-      external_sm_name_key      = "MB_DB_DBNAME"
-      k8s_property_key = "database_name"
+      external_sm_name     = "tf-metabase-db-secret"
+      external_sm_name_key = "MB_DB_DBNAME"
+      k8s_property_key     = "database_name"
     }
   ]
 
