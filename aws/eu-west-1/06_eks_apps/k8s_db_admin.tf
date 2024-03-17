54ac459f5c486fd32c0b80b33761718e75f2a00c
mustafaalaa.mohamed@gmail.com <mustafaalaa.mohamed@gmail.com>
2024-03-17 03:18:51 +0000

    ADD: DB admin automation

---
+++ aws/eu-west-1/06_eks_apps/k8s_db_admin.tf
@@ -0,0 +1,96 @@
+resource "kubernetes_manifest" "namespace_db" {
+  manifest = {
+    "apiVersion" = "v1"
+    "kind"       = "Namespace"
+    "metadata"   = {
+      "labels" = {
+        "app"                        = var.db_namespace_name
+        "app.kubernetes.io/instance" = var.db_namespace_name
+        "app.kubernetes.io/name"     = var.db_namespace_name
+      }
+      "name" = var.db_namespace_name
+    }
+  }
+}
+
+resource "kubernetes_manifest" "k8s_serviceaccount_db_rds_admin" {
+  depends_on = [kubernetes_manifest.namespace_db]
+  manifest   = {
+    "apiVersion"                   = "v1"
+    "automountServiceAccountToken" = true
+    "kind"                         = "ServiceAccount"
+    "metadata"                     = {
+      "annotations" = {
+        "eks.amazonaws.com/role-arn" = "arn:aws:iam::730335474513:role/tf-datasquad-eks-rds-admin-irsa" #TODO: to be automated
+      }
+      "labels" = {
+        "app.kubernetes.io/instance" = var.db_service_account_name
+        "app.kubernetes.io/name"     = var.db_service_account_name
+      }
+      "name"      = var.db_service_account_name
+      "namespace" = kubernetes_manifest.namespace_db.manifest.metadata.name
+    }
+  }
+}
+#
+resource "kubernetes_manifest" "k8s_secretstore_db_admin_external_store" {
+  depends_on = [kubernetes_manifest.k8s_serviceaccount_db_rds_admin]
+  manifest   = {
+    "apiVersion" = "external-secrets.io/v1beta1"
+    "kind"       = "SecretStore"
+    "metadata"   = {
+      "name"      = var.db_external_secret_store_name
+      "namespace" = kubernetes_manifest.namespace_db.manifest.metadata.name
+    }
+    "spec" = {
+      "provider" = {
+        "aws" = {
+          "auth" = {
+            "jwt" = {
+              "serviceAccountRef" = {
+                "name" = kubernetes_manifest.k8s_serviceaccount_db_rds_admin.manifest.metadata.name
+              }
+            }
+          }
+          "region"  = var.region
+          "service" = "SecretsManager"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "configmap_db_postgres_init_sql" {
+  manifest = {
+    "apiVersion" = "v1"
+    "data" = {
+      "init.sql" = <<-EOT
+      CREATE USER :database_user WITH PASSWORD :'database_password';
+      CREATE DATABASE :database_name;
+      GRANT ALL PRIVILEGES ON DATABASE :database_name TO :database_user;
+      EOT
+    }
+    "kind" = "ConfigMap"
+    "metadata" = {
+      "name" = "postgres-init-sql"
+      "namespace" = var.db_namespace_name
+    }
+  }
+}
+
+resource "kubernetes_manifest" "configmap_db_postgres_db_drop" {
+  manifest = {
+    "apiVersion" = "v1"
+    "data" = {
+      "drop.sql" = <<-EOT
+      DROP DATABASE IF EXISTS :database_name;
+      DROP USER IF EXISTS :database_user;
+      EOT
+    }
+    "kind" = "ConfigMap"
+    "metadata" = {
+      "name" = "postgres-db-drop"
+      "namespace" = var.db_namespace_name
+    }
+  }
+}
---
+++ aws/eu-west-1/06_eks_apps/k8s_secret_creation.tf
@@ -0,0 +1,63 @@
+module "rds_admin_k8s_external_secret" {
+  depends_on           = [kubernetes_manifest.k8s_secretstore_db_admin_external_store]
+  source               = "../../../k8s/modules/db_provisioner"
+  external_secret_name = "${local.name_prefix}-rds-admin-external-secret"
+  secret_map           = [
+    {
+      external_sm_name          = "tf-petclinic-db-secret"
+      external_sm_name_key      = "POSTGRES_HOST"
+      k8s_property_key = "PGHOST"
+    },
+    {
+      external_sm_name          = "tf-petclinic-db-secret"
+      external_sm_name_key      = "PGDATABASE"
+      k8s_property_key = "POSTGRES_DB"
+    },
+    {
+      external_sm_name          = "tf-petclinic-db-secret"
+      external_sm_name_key      = "POSTGRES_PORT"
+      k8s_property_key = "PGPORT"
+    },
+    {
+      external_sm_name          = "rds!db-390eb28a-2563-4d84-b9f1-9b3205f59365"
+      external_sm_name_key      = "POSTGRES_PORT"
+      k8s_property_key = "password"
+    },
+    {
+      external_sm_name          = "rds!db-390eb28a-2563-4d84-b9f1-9b3205f59365"
+      external_sm_name_key      = "POSTGRES_PORT"
+      k8s_property_key = "username"
+    }
+  ]
+
+  refresh_rate               = "1h"
+  external_secret_store_name = kubernetes_manifest.k8s_secretstore_db_admin_external_store.manifest.metadata.name
+  namespace_name             = kubernetes_manifest.namespace_db.manifest.metadata.name
+
+}
+module "metabase_k8s_external_secret" {
+  depends_on           = [kubernetes_manifest.k8s_secretstore_db_admin_external_store]
+  source               = "../../../k8s/modules/db_provisioner"
+  external_secret_name = "${local.name_prefix}-metabase-external-secret"
+  secret_map           = [
+    {
+      external_sm_name          = "tf-metabase-db-secret"
+      external_sm_name_key      = "MB_DB_USER"
+      k8s_property_key = "database_user"
+    },
+    {
+      external_sm_name          = "tf-metabase-db-secret"
+      external_sm_name_key      = "MB_DB_PASS"
+      k8s_property_key = "database_password"
+    },
+    {
+      external_sm_name          = "tf-metabase-db-secret"
+      external_sm_name_key      = "MB_DB_DBNAME"
+      k8s_property_key = "database_name"
+    }
+  ]
+
+  refresh_rate               = "1h"
+  external_secret_store_name = kubernetes_manifest.k8s_secretstore_db_admin_external_store.manifest.metadata.name
+  namespace_name             = kubernetes_manifest.namespace_db.manifest.metadata.name
+}
