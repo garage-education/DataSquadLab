cd6f856e85aede2b52bfddbda8aacdd1870f70e7
mustafaalaa.mohamed@gmail.com <mustafaalaa.mohamed@gmail.com>
2024-03-18 02:30:40 +0000

    revamp metabase

--- aws/eu-west-1/06_eks_apps/variables.tf
+++ aws/eu-west-1/06_eks_apps/variables.tf
@@ -10,3 +10,4 @@ variable "db_service_account_name" { default = "rds-admin-external-secret-sa"}
 variable "db_external_secret_store_name" { default = "tf-datasquad-rds-db-store"}
 variable "metabase_namespace_name" {default = "metabase"}
 variable "metabase_service_account_name" { default = "tf-datasquad-eks-metabase-sa"}
+variable "metabase_external_secret_store_name" { default = "tf-datasquad-metabase-db-store"}
