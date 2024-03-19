ffb8e8db608bf0484873495c5b8196b9a160d2e4
mustafaalaa.mohamed@gmail.com <mustafaalaa.mohamed@gmail.com>
2024-03-19 22:30:27 +0000

    Split K8s backend apps into a separate folder for argo deployment

---
+++ aws/eu-west-1/07_eks_be_apps/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  name_prefix                       = "${var.prefix}${var.name}-eks"
+
+  env = {
+    account_id  = data.aws_caller_identity.current.account_id
+    region      = data.aws_region.current.name
+    environment = var.environment
+  }
+
+  default_tags = {
+    Region      = data.aws_region.current.name
+    Environment = var.environment
+    Owner       = "INFRA_TEAM"
+    Project     = "INFRA_PROJECT"
+    Stage       = "KUBERNETES"
+    ManagedBy   = var.ManagedBy
+    CostCenter  = "APP"
+  }
+}
