e7ba464d560678508bb383f3296ce4da612cffdc
Ahmed Hassanien <ahmed_hassanien@epam.com>
2024-04-14 12:21:25 +0200

    UPDATE: Moved infra applications to ArgoCD instead of Terraform.

--- aws/eu-west-1/21_eks_infra_apps/cluster_helm_apps.tf
+++ aws/eu-west-1/21_eks_infra_apps/cluster_helm_apps.tf
@@ -1,33 +1,5 @@
-resource "helm_release" "nginx_ingress" {
-  name             = "ingress-nginx"
-  repository       = "https://kubernetes.github.io/ingress-nginx"
-  chart            = "ingress-nginx"
-  namespace        = "ingress-nginx"
-  version          = "4.9.1"
-  create_namespace = true
-  atomic           = true
-
-  values = [
-    file("helm_values/ingress-nginx.yaml")
-  ]
-}
-
-resource "helm_release" "cert_manager" {
-  name             = "cert-manager"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  namespace        = "cert-manager"
-  version          = "1.13.3"
-  create_namespace = true
-
-  values = [
-    file("helm_values/cert-manager.yaml")
-  ]
-}
-
 ####
 resource "helm_release" "argo_cd" {
-  depends_on       = [helm_release.nginx_ingress,helm_release.cert_manager,kubernetes_manifest.clusterissuer_letsencrypt_production]
   name             = "argo-cd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
@@ -37,16 +9,4 @@ resource "helm_release" "argo_cd" {
   values           = [
     file("helm_values/argocd.yaml")
   ]
-}
-
-##eso
-resource "helm_release" "eso" {
-  name             = "external-secrets"
-  chart            = "external-secrets"
-  namespace        = "external-secrets"
-  repository       = "https://charts.external-secrets.io"
-  version          = "0.9.11"
-  timeout          = 300
-  atomic           = true
-  create_namespace = true
 }
